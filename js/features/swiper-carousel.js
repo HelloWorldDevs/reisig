5f5ddd2414635ce06c3d73986aa768d43b2a6aae
robertgraleigh <robertgraleigh@gmail.com>
2016-10-11 15:45:27 -0700

    updated slider overflow

--- js/features/swiper-carousel.js
+++ js/features/swiper-carousel.js
@@ -1,7 +1,7 @@
 /* controls the swiper carousel feature at the top of site */
 $(document).ready(function () {
 	//initialize swiper when document ready  
-	var bannerSwiper = new Swiper ('.banner-swiper-container', {
+	var bannerSwiper = new Swiper ('#banner-swiper-container', {
 		// Optional parameters
 		direction: 'horizontal',
 		speed: 1000,
@@ -9,6 +9,7 @@ $(document).ready(function () {
 		effect: 'slide',
 		loop: true,
 		grabCursor: true,
+		simulateTouch: true,
 		paginationHide: true,
 		scrollbarHide: true
 	});        
