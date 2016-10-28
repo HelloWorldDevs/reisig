5b4c22b195f2aa142c16416da918eac6f3c92e49
robertgraleigh <robertgraleigh@gmail.com>
2016-10-28 10:42:43 -0700

    updated swiper carousel feature

--- js/features/swiper-carousel.js
+++ js/features/swiper-carousel.js
@@ -1,7 +1,11 @@
+/* ==========================================
+	PRODUCTION SWIPER CAROUSEL FEATURE SCRIPT
+========================================== */
+
 /* controls the swiper carousel feature at the top of site */
 $(document).ready(function () {
 	//initialize swiper when document ready  
-	var bannerSwiper = new Swiper ('#banner-swiper-container', {
+	var bannerSwiper = new Swiper ('#swiper-carousel-container', {
 		// Optional parameters
 		direction: 'horizontal',
 		speed: 1000,
