7bfd02602c6a4ef39c46b1cb22310f4324b65b23
robertgraleigh <robertgraleigh@gmail.com>
2016-11-02 17:48:25 -0700

    updated nav

--- js/features/navigation.js
+++ js/features/navigation.js
@@ -1,6 +1,12 @@
+/*
 (function(){
 	var primaryNav = $("#primary-navigation");
 	primaryNav.on("click", "a", null, function () {
 		primaryNav.collapse("hide");
 	});
 }());
+
+$('.navbar-collapse a').click(function(){
+	$(".navbar-collapse").collapse('hide');
+});
+*/
